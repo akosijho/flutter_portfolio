41a0b01bcf75067ce9e5d56c5fd337c0a5abc35b
akosijho <jhobertpanerio@gmail.com>
2023-10-17 08:11:53 +0800

    Created body background.

--- lib/app/sections/main/main_page.dart
+++ lib/app/sections/main/main_page.dart
@@ -1,8 +1,14 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_portfolio/app/sections/main/widgets/widgets.dart';
+import 'package:my_portfolio/core/color/colors.dart';
 import 'package:my_portfolio/core/configs/app.dart';
 import 'package:my_portfolio/core/providers/drawer_provider.dart';
 import 'package:my_portfolio/core/responsive/responsive.dart';
+import 'package:my_portfolio/core/theme/cubit/theme_cubit.dart';
+import 'package:my_portfolio/util/util.dart';
 import 'package:provider/provider.dart';
 
 class MainPage extends StatelessWidget {
@@ -25,7 +31,62 @@ class MainPage extends StatelessWidget {
             mobile: NavBarTablet(),
           )),
       drawer: !ResponsiveWidget.isDesktop(context) ? const MobileDrawer() : null,
-      body: const Placeholder(),
+      body: BlocBuilder<ThemeCubit, ThemeState>(
+        builder: (context, state) {
+          return Stack(
+            children: [
+              Positioned(
+                height: height * 0.2,
+                left: -88,
+                child: Container(
+                  height: height / 3,
+                  width: 166,
+                  decoration: const BoxDecoration(shape: BoxShape.circle, color: secondaryColor),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 200, sigmaY: 200),
+                    child: Container(
+                      height: 166,
+                      width: 166,
+                      color: Colors.transparent,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                  bottom: 0,
+                  right: -100,
+                  child: Container(
+                    height: 100,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: primaryColor.withOpacity(0.5),
+                    ),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaY: 500, sigmaX: 500),
+                      child: Container(
+                        height: 200,
+                        width: 200,
+                        color: Colors.transparent,
+                      ),
+                    ),
+                  )),
+              if (state.isDarkThemeOn)
+                Align(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    Images.cosmos,
+                    opacity: const AlwaysStoppedAnimation<double>(0.2),
+                    width: width,
+                    height: height,
+                    fit: BoxFit.cover,
+                    alignment: Alignment.topCenter,
+                  ),
+                ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
--- lib/util/images.dart
+++ lib/util/images.dart
@@ -1,6 +1,6 @@
 class Images {
   Images._();
 
-  static const String _rootPath = "assets/images/";
+  static const String _rootPath = "images/";
   static const String cosmos = "${_rootPath}5424482.jpg";
 }
